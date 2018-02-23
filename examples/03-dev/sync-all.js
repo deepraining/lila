46cdf7d73112695df1e61546e9bb584b039f32c3
senntyou <jiangjinbelief@163.com>
2018-02-23 17:23:08 +0800

    complete sync cmd for 0.1.0

--- examples/03-dev/sync-all.js
+++ examples/03-dev/sync-all.js
@@ -4,13 +4,9 @@ require('../../util/change_cwd_to')(__dirname + '/demo');
 var servers = require('./servers');
 
 var projectConfig = require('./demo/lila.config');
-projectConfig.htmlAbsoluteAndCdnPath = !0;
 projectConfig.network = [];
 projectConfig.network[100] = {
     useSsh: !0,
-    staticDomain: 'http://www.baidu.com/lila',
-    cssAbsolutePathPrefix: '/lila',
-    backupHtml: !0,
     servers: servers
 };
 
