d1569b1abf3e89c394f68397ef34391c571954be
senntyou <jiangjinbelief@163.com>
2018-04-18 14:16:53 +0800

    update clean test example

--- examples/08-clean/demo/bak/abcdef123456789abcdef123456789aa.js
+++ examples/08-clean/demo/bak/abcdef123456789abcdef123456789aa.js
@@ -1,3 +1,4 @@
 /**
  * @author senntyou <jiangjinbelief@163.com>
  */
+var a = {"19": "721ea8e8a5ae693fd7ed70b501c7d244"};
--- examples/08-clean/demo/bak/abcdef123456789abcdef123456789bb.js
+++ examples/08-clean/demo/bak/abcdef123456789abcdef123456789bb.js
@@ -1,3 +1,4 @@
 /**
  * @author senntyou <jiangjinbelief@163.com>
  */
+var a = {"9": "7b7c4210539c2c41354207f419ec0243"};
