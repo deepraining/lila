2e7617fa190f9c9f27d9ffb5a353a4a6a46e3c8c
senntyou <jiangjinbelief@163.com>
2018-04-19 16:23:01 +0800

    update

---
+++ examples/03-dev/demo/project/src/test-5/index/config.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    packCssSeparately: !0
+};
--- examples/03-dev/dist.js
+++ examples/03-dev/dist.js
@@ -1,4 +1,5 @@
 
 require('../../util/change_cwd_to')(__dirname + '/demo');
 
-require('../../util/exec')('lila dist test-5/index -l senn');
+// require('../../util/exec')('lila dist test-5/index -l senn -o');
+require('../../util/exec')('lila dist test-5/index');
