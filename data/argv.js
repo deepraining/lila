555465b4966461b66efad602745e3fd72926040a
senntyou <jiangjinbelief@163.com>
2018-07-09 17:23:31 +0800

    update

--- data/argv.js
+++ data/argv.js
@@ -1,7 +1,7 @@
 const argv = require('minimist')(process.argv.slice(2));
 
 // need `--module`
-const commandsNeedModuleArg = ['dev', 'dist', 'sync', 'analyze', 'ana', 'eslint', 'stylelint', 'prettier', 'upgrade'];
+const commandsNeedModuleArg = ['dev', 'dist', 'sync', 'analyze', 'ana', 'eslint', 'stylelint', 'prettier'];
 
 // command name
 const commandName = argv._[0];
