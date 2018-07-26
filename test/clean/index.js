496cb163e2a0683861c78e45d5d1fda99af5b98e
senntyou <jiangjinbelief@163.com>
2018-07-26 18:53:31 +0800

    delete test/archive,clean,revert

--- test/clean/index.js
+++
@@ -1,7 +0,0 @@
-const fse = require('fs-extra');
-
-fse.copySync(__dirname + '/demo/bak', __dirname + '/demo/dist');
-
-require('../../util/change_cwd')(__dirname + '/demo');
-
-require('../../util/exec')('lila clean');
