9611fc7b3f94b7f3e389d82ae512df65e7507a1e
senntyou <jiangjinbelief@163.com>
2019-02-22 20:18:53 +0800

    prettier

--- packages/lila-webpack-config/example/app/src/jsx/hello.jsx
+++ packages/lila-webpack-config/example/app/src/jsx/hello.jsx
@@ -1,15 +1,19 @@
-import React, {Component} from 'react';
-import {Button} from 'antd';
+import React, { Component } from 'react';
+import { Button } from 'antd';
 import styles from './hello.less';
 
 export default class extends Component {
   render() {
+    const { name } = this.props;
+
     return (
       <div>
-        <button type="button" className={styles.btn} style={{marginRight: '20px'}}>
-          Hello,
-          {' '}
-          {this.props.name}
+        <button
+          type="button"
+          className={styles.btn}
+          style={{ marginRight: '20px' }}
+        >
+          Hello, {name}
         </button>
         <Button>antd</Button>
       </div>
--- packages/lila-webpack-lib-config/example/app/src/hello.jsx
+++ packages/lila-webpack-lib-config/example/app/src/hello.jsx
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './hello.less';
 
 export default class extends Component {
   render() {
+    const { name } = this.props;
+
     return (
-      <button type="button" className='jsx-btn'>
-        Hello,
-        {' '}
-        {this.props.name}
+      <button type="button" className="jsx-btn">
+        Hello, {name}
       </button>
     );
   }
